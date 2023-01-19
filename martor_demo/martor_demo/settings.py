22d4159fc238e395383fbc67083e2180319eb104
Ruslan Poltayev <ruslan@chocotravel.com>
2023-01-19 15:38:42 +0600

    change static directory name

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -142,5 +142,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'martor_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'martor_media')
